b62d8672bb084388deb737c955c2a406cdb0d093
AugustoMagalhaes <augustommg@gmail.com>
2021-12-22 04:06:43 -0300

    Adicionando resolucao do requisito 7.

--- script.js
+++ script.js
@@ -11,8 +11,21 @@ function createPixelBoard(boardSize) {
 createPixelBoard(25);
 
 const pixelsList = document.getElementsByClassName('pixel');
+const colorsList = document.getElementsByClassName('color');
 
 window.onload = function () {
     const firstColorPalette = document.getElementsByClassName('color')[0];
     firstColorPalette.className += ' ' + 'selected';
 };
+
+function paletteClickColor(event) {
+    const selectedElement = event.target;    
+    for (const element of colorsList) {
+        element.className = 'color';
+    }
+    selectedElement.className = 'color selected';
+}
+
+for (const colorsListItem of colorsList) {
+    colorsListItem.addEventListener('click', paletteClickColor);
+}
